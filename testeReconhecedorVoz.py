cf328497ae6376042b0c33670efb1c7df8328957
Victor Mello <victorsmello93@gmail.com>
2020-03-17 23:47:35 -0300

    Removendo o acento devido ao bug no encoding do code runner que não está em utf-8

--- testeReconhecedorVoz.py
+++ testeReconhecedorVoz.py
@@ -1,16 +1,15 @@
-
 import speech_recognition as sr
 
 r = sr.Recognizer()
 
 #Utilizando microfone para capturar voz
-with sr.Microphone as fonte:
+with sr.Microphone() as fonte:
     print('Diga alguma coisa: ')
     audio = r.listen(fonte) #Captura de voz
 
     texto = r.recognize_google(audio)
 
     try:
-        print('Você disse: ' + texto)
+        print('Voce disse: ' + texto)
     except:
         print('Não foi possível compreender o que você disse')
