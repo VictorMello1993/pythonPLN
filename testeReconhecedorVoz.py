69dc2dd6b400faae15dfcf45062915a4188e524c
Victor Mello <victorsmello93@gmail.com>
2020-03-18 21:36:16 -0300

    Implementada a função de gravar a voz reconhecido em um arquivo de áudio.

--- testeReconhecedorVoz.py
+++ testeReconhecedorVoz.py
@@ -5,11 +5,13 @@ r = sr.Recognizer()
 #Utilizando microfone para capturar voz
 with sr.Microphone() as fonte:
     print('Diga alguma coisa: ')
-    audio = r.listen(fonte) #Captura de voz
+    while True:
+        audio = r.listen(fonte) #Captura de voz
+        texto = r.recognize_google(audio, 'pt-BR') 
 
-    texto = r.recognize_google(audio)
-
-    try:
-        print('Voce disse: ' + texto)
-    except:
-        print('Não foi possível compreender o que você disse')
+        with open('voz.wav', 'wb') as arquivo:
+            arquivo.write(audio.get_wav_data()) #Salvando a voz reconhecida em um arquivo wav
+        try:
+            print('Voce disse: ' + texto)
+        except:
+            print('Não foi possível compreender o que você disse')
