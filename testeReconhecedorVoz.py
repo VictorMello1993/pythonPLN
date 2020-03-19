fa2cf1a2c7d4f48cf5691b5875333337a10a30f5
Victor Mello <victorsmello93@gmail.com>
2020-03-18 23:07:20 -0300

    Reconhecedor de voz na versão em português

--- testeReconhecedorVoz.py
+++ testeReconhecedorVoz.py
@@ -2,16 +2,16 @@ import speech_recognition as sr
 
 r = sr.Recognizer()
 
-#Utilizando microfone para capturar voz
+# Utilizando microfone como fonte de áudio
 with sr.Microphone() as fonte:
+    r.adjust_for_ambient_noise(fonte)  # Cancelando ruídos
     print('Diga alguma coisa: ')
     while True:
-        audio = r.listen(fonte) #Captura de voz
-        texto = r.recognize_google(audio, 'pt-BR') 
-
-        with open('voz.wav', 'wb') as arquivo:
-            arquivo.write(audio.get_wav_data()) #Salvando a voz reconhecida em um arquivo wav
+        audio = r.listen(fonte)  # Captura de voz
+        texto = r.recognize_google(audio, language='pt-BR')
         try:
-            print('Voce disse: ' + texto)
-        except:
+            print('Você disse: ' + texto)
+        except sr.UnknownValueError:
             print('Não foi possível compreender o que você disse')
+        except sr.RequestError as e:
+            print('Erro ao executar recognize_google, {0}'.format(e))
